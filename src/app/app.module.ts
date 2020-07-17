12c62c7840989b301a88cf535cad91ea75fb7e7d
viraj02 <virujoshi02@gmail.com>
2020-07-18 01:16:15 +0530

    Deleted ModulesComponent import as it was bymistake created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ModulesComponent } from './modules/modules.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ModulesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
