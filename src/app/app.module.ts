74fb496cc280452c7ff3346ffc8cb13cdda39c51
viraj02 <virujoshi02@gmail.com>
2020-07-18 00:36:30 +0530

    created modules folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModulesComponent } from './modules/modules.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModulesComponent
   ],
   imports: [
     BrowserModule,
