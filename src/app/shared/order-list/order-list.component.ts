842bd41d1f00e1437b7d2d907ca721ae1841183e
viraj02 <virujoshi02@gmail.com>
2020-08-12 20:33:46 +0530

    Todolist button and dialog component added

--- src/app/shared/order-list/order-list.component.ts
+++ src/app/shared/order-list/order-list.component.ts
@@ -1,50 +1,60 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { DialogService } from 'primeng/api';
+import { DialogContentComponent } from '../dialog-content/dialog-content.component';
 
 @Component({
   selector: 'app-prime-order-list',
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.css'],
+  providers: [DialogService],
   encapsulation: ViewEncapsulation.None
 })
 export class OrderListComponent implements OnInit {
 
   cars: any[];
 
-  constructor() { }
+  constructor(public dialogService: DialogService, ) { }
 
   ngOnInit() {
     this.cars = [
-      { "brand": "VW", "year": 2012, "color": "Orange", "vin": "dsad231ff" },
-      { "brand": "Audi", "year": 2011, "color": "Black", "vin": "gwregre345" },
-      { "brand": "Renault", "year": 2005, "color": "Gray", "vin": "h354htr" },
-      { "brand": "BMW", "year": 2003, "color": "Blue", "vin": "j6w54qgh" },
-      { "brand": "Mercedes", "year": 1995, "color": "Orange", "vin": "hrtwy34" },
-      { "brand": "Volvo", "year": 2005, "color": "Black", "vin": "jejtyj" },
-      { "brand": "Honda", "year": 2012, "color": "Yellow", "vin": "g43gr" },
-      { "brand": "Jaguar", "year": 2013, "color": "Orange", "vin": "greg34" },
-      { "brand": "Ford", "year": 2000, "color": "Black", "vin": "h54hw5" },
-      { "brand": "Fiat", "year": 2013, "color": "Red", "vin": "245t2s" },
-      { "brand": "VW", "year": 2012, "color": "Orange", "vin": "dsad231ff" },
-      { "brand": "Audi", "year": 2011, "color": "Black", "vin": "gwregre345" },
-      { "brand": "Renault", "year": 2005, "color": "Gray", "vin": "h354htr" },
-      { "brand": "BMW", "year": 2003, "color": "Blue", "vin": "j6w54qgh" },
-      { "brand": "Mercedes", "year": 1995, "color": "Orange", "vin": "hrtwy34" },
-      { "brand": "Volvo", "year": 2005, "color": "Black", "vin": "jejtyj" },
-      { "brand": "Honda", "year": 2012, "color": "Yellow", "vin": "g43gr" },
-      { "brand": "Jaguar", "year": 2013, "color": "Orange", "vin": "greg34" },
-      { "brand": "Ford", "year": 2000, "color": "Black", "vin": "h54hw5" },
-      { "brand": "Fiat", "year": 2013, "color": "Red", "vin": "245t2s" },
-      { "brand": "VW", "year": 2012, "color": "Orange", "vin": "dsad231ff" },
-      { "brand": "Audi", "year": 2011, "color": "Black", "vin": "gwregre345" },
-      { "brand": "Renault", "year": 2005, "color": "Gray", "vin": "h354htr" },
-      { "brand": "BMW", "year": 2003, "color": "Blue", "vin": "j6w54qgh" },
-      { "brand": "Mercedes", "year": 1995, "color": "Orange", "vin": "hrtwy34" },
-      { "brand": "Volvo", "year": 2005, "color": "Black", "vin": "jejtyj" },
-      { "brand": "Honda", "year": 2012, "color": "Yellow", "vin": "g43gr" },
-      { "brand": "Jaguar", "year": 2013, "color": "Orange", "vin": "greg34" },
-      { "brand": "Ford", "year": 2000, "color": "Black", "vin": "h54hw5" },
-      { "brand": "Fiat", "year": 2013, "color": "Red", "vin": "245t2s" }
+      { brand: 'VW', year: 2012, color: 'Orange', vin: 'dsad231ff' },
+      { brand: 'Audi', year: 2011, color: 'Black', vin: 'gwregre345' },
+      { brand: 'Renault', year: 2005, color: 'Gray', vin: 'h354htr' },
+      { brand: 'BMW', year: 2003, color: 'Blue', vin: 'j6w54qgh' },
+      { brand: 'Mercedes', year: 1995, color: 'Orange', vin: 'hrtwy34' },
+      { brand: 'Volvo', year: 2005, color: 'Black', vin: 'jejtyj' },
+      { brand: 'Honda', year: 2012, color: 'Yellow', vin: 'g43gr' },
+      { brand: 'Jaguar', year: 2013, color: 'Orange', vin: 'greg34' },
+      { brand: 'Ford', year: 2000, color: 'Black', vin: 'h54hw5' },
+      { brand: 'Fiat', year: 2013, color: 'Red', vin: '245t2s' },
+      { brand: 'VW', year: 2012, color: 'Orange', vin: 'dsad231ff' },
+      { brand: 'Audi', year: 2011, color: 'Black', vin: 'gwregre345' },
+      { brand: 'Renault', year: 2005, color: 'Gray', vin: 'h354htr' },
+      { brand: 'BMW', year: 2003, color: 'Blue', vin: 'j6w54qgh' },
+      { brand: 'Mercedes', year: 1995, color: 'Orange', vin: 'hrtwy34' },
+      { brand: 'Volvo', year: 2005, color: 'Black', vin: 'jejtyj' },
+      { brand: 'Honda', year: 2012, color: 'Yellow', vin: 'g43gr' },
+      { brand: 'Jaguar', year: 2013, color: 'Orange', vin: 'greg34' },
+      { brand: 'Ford', year: 2000, color: 'Black', vin: 'h54hw5' },
+      { brand: 'Fiat', year: 2013, color: 'Red', vin: '245t2s' },
+      { brand: 'VW', year: 2012, color: 'Orange', vin: 'dsad231ff' },
+      { brand: 'Audi', year: 2011, color: 'Black', vin: 'gwregre345' },
+      { brand: 'Renault', year: 2005, color: 'Gray', vin: 'h354htr' },
+      { brand: 'BMW', year: 2003, color: 'Blue', vin: 'j6w54qgh' },
+      { brand: 'Mercedes', year: 1995, color: 'Orange', vin: 'hrtwy34' },
+      { brand: 'Volvo', year: 2005, color: 'Black', vin: 'jejtyj' },
+      { brand: 'Honda', year: 2012, color: 'Yellow', vin: 'g43gr' },
+      { brand: 'Jaguar', year: 2013, color: 'Orange', vin: 'greg34' },
+      { brand: 'Ford', year: 2000, color: 'Black', vin: 'h54hw5' },
+      { brand: 'Fiat', year: 2013, color: 'Red', vin: '245t2s' }
     ]
   }
+
+  show() {
+    const ref = this.dialogService.open(DialogContentComponent, {
+        header: 'Choose a Car',
+        width: '70%'
+    });
+}
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderListComponent } from './order-list/order-list.component';
 import { OrderListModule } from 'primeng/orderlist';
+import { ToolbarModule } from 'primeng/toolbar';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { DialogComponent } from './dialog/dialog.component';
+import { DialogContentComponent } from './dialog-content/dialog-content.component';
+
 
 
 
 @NgModule({
-  declarations: [OrderListComponent,   ],
+  declarations: [OrderListComponent, DialogComponent, DialogContentComponent,
+  ],
   imports: [
     CommonModule,
-    OrderListModule
+    DynamicDialogModule,
+    OrderListModule,
+    ToolbarModule
   ],
   exports: [
     OrderListComponent,
+    DialogComponent
+  ],
+  entryComponents: [
+    DialogContentComponent,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA,
   ]
 })
 export class SharedModule { }
